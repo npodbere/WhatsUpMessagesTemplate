19ba7547717df5890f2560f6cf5ad7dd49a93142
Nikita Podberezkin <nikita@brilliant.tech>
2024-06-15 14:29:36 +0300

    Initial sass set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+
+//main body reducer: {
+//  components: [
+//  {header reducer}
+//  {body reducer}
+//  {footer reducer}
+//  {buttons reducer}
+//]
+//}
+
 
 function App() {
   return (
